e2438f0152ff61cbcd5b34a8d386196ecdaf6e1b
Pon-Taichi <saramanda164@gmail.com>
2021-11-08 20:30:23 +0900

    チュートリアル第2章リストの表示が終了

---
+++ angular-tour-of-heroes/src/app/mock-heros.ts
@@ -0,0 +1,14 @@
+import { Hero } from "./hero";
+
+export const HEROES: Hero[] = [
+    { id: 11, name: "Dr Nice" },
+    { id: 12, name: "Narco" },
+    { id: 13, name: "Bob" },
+    { id: 14, name: "Chris Mass" },
+    { id: 15, name: "Dunk" },
+    { id: 16, name: "Espacito" },
+    { id: 17, name: "Fled" },
+    { id: 18, name: "Gerorgina" },
+    { id: 19, name: "Harry Kane" },
+    { id: 20, name: "Iden" },
+];
